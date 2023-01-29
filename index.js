866279ffed7fb1de64cb16c2d2011f75406f101b
Manuel <manuel.uzcategui@aictive.co>
2023-01-29 20:10:38 -0300

    - Adding cors configuration for develop

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const routes = require("./routes");
 const server = require("http").Server(app);
@@ -6,9 +7,14 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 3500;
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 server.listen(port);
